fee2e2c16510bf5b60a6060621541fa313523758
simke9445 <simke9445@gmail.com>
2023-08-21 15:31:55 +0200

    add orne example

---
+++ src/example_orne.ts
@@ -0,0 +1,79 @@
+export { TerraTxError } from './wallet/utils';
+import { env } from 'process';
+import { base64encode } from './utils';
+import { LCDClient, LCDClientConfig, MnemonicKey, Wallet } from '@terra-money/feather.js';
+import { WarpSdk } from './sdk';
+import dotenv from 'dotenv';
+import { uint, cond, fn, msg, variable, job, ts } from './composers';
+
+dotenv.config();
+
+const mainnetConfig: Record<string, LCDClientConfig> = {
+  'phoenix-1': {
+    chainID: 'phoenix-1',
+    lcd: 'https://phoenix-lcd.terra.dev',
+    gasAdjustment: 1.75,
+    gasPrices: { uluna: 0.15 },
+    prefix: 'terra',
+  },
+};
+
+const testnetConfig: Record<string, LCDClientConfig> = {
+  'pisco-1': {
+    lcd: 'https://pisco-lcd.terra.dev',
+    chainID: 'pisco-1',
+    gasAdjustment: 1.75,
+    gasPrices: { uluna: 0.15 },
+    prefix: 'terra',
+  },
+};
+
+const lcd = new LCDClient(env.NETWORK === 'mainnet' ? mainnetConfig : testnetConfig);
+
+const chainId = env.NETWORK === 'mainnet' ? 'phoenix-1' : 'pisco-1';
+
+const lcdClientConfig = lcd.config[chainId];
+
+const wallet = new Wallet(lcd, new MnemonicKey({ mnemonic: env.MNEMONIC_KEY }));
+
+const sdk = new WarpSdk(wallet, lcd.config[chainId]);
+
+const sender = wallet.key.accAddress(lcdClientConfig.prefix);
+
+const nextExecution = variable
+  .static()
+  .kind('uint')
+  .name('next_execution')
+  .value(ts.date(new Date('2023-08-20T12:30:00.000Z')))
+  .onSuccess(fn.uint(uint.expr(uint.simple(ts.days(1)), 'add', uint.env('time'))))
+  .onError(fn.uint(uint.expr(uint.simple(ts.hours(1)), 'add', uint.env('time'))))
+  .compose();
+
+const condition = cond.uint(uint.env('time'), 'gt', uint.ref(nextExecution));
+
+const createJobMsg = job
+  .create()
+  .name('orne-dao--staking-rewards')
+  .recurring(true)
+  .requeueOnEvict(true)
+  .reward('50000')
+  .description('This job distributes rewards to OrneDAO stakers each day.')
+  .labels([])
+  .cond(condition)
+  .var(nextExecution)
+  .msg(
+    msg.execute('terra1k8k32t9fd6v5smjsnymv97sywdl2jppmlhaqw6nm63zq332jmmms9kgk26', {
+      send: {
+        amount: '250000000000000', // orne token is 6 decimals
+        contract: 'terra19p20mfnvwh9yvyr7aus3a6z6g6uk28fv4jhx9kmnc2m7krg27q2qkfenjw',
+        msg: base64encode({ distribute: {} }),
+      },
+    })
+  )
+  .compose();
+
+console.log({ createJobMsg: JSON.stringify(createJobMsg) });
+
+sdk.createJob(sender, createJobMsg).then((response) => {
+  console.log(response);
+});
